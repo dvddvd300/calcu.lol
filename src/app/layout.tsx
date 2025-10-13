6ffbda496908bc50716df2fab3c81eba322ebb7f
LaCumbreCotidiana <21372070+dvddvd300@users.noreply.github.com>
2025-10-12 23:13:20 -0600

    GA

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-NJQRF5F5ZM"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-NJQRF5F5ZM');
+          `,
+          }}
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
